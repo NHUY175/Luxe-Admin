0f8eef7507b6a59e54578cd5cd22155958045667
yenvy031110 <141745084+yenvy031110@users.noreply.github.com>
2024-04-09 23:46:35 +0700

    abc

--- nhanvien.php
+++ nhanvien.php
@@ -8,12 +8,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&dinvlay=swap"
       rel="stylesheet"
     />
-    <!-- Styles -->
-    <link rel="stylesheet" href="css/reset.css" />
-    <link rel="stylesheet" href="css/nhanvien.css" />
+    <!-- File reset đảm bảo kích thước hiển thị website giữa các trình duyệt khác nhau -->
+  <link rel="stylesheet" href="css/reset.css" />
+  <!-- File định dạng các page sản phẩm -->
+  <link rel="stylesheet" href="css/nhanvien.css" />
     <!-- Scripts -->
     <script src="./js/scripts.js"></script>
   </head>
@@ -25,28 +26,41 @@
       include "header.php";   
     ?>
     <?php
+    $link = null;
+    taoKetNoi($link);
+    //Kết nối và lấy dữ liệu từ CSDL > Tiến hành phân trang
+  //Số lượng NV
+  $nhanvien = chayTruyVanTraVeDL($link, "SELECT COUNT(*) AS so_luong_nhan_vien FROM tbl_nhanvien");
+  $so_luong_nhan_vien = mysqli_fetch_assoc($nhanvien)["so_luong_nhan_vien"];
+  // Tính toán số trang
+  $so_trang = ceil($so_luong_nhan_vien / 10);
+  giaiPhongBoNho($link, $so_luong_nhan_vien);
+  ?>
     <!-- Page Title -->
     <section id="page-title">
       <div class="container">
         <div class="home-title">
           <img src="./icon/nhanvien-diamond-2.svg" alt="" />
           <h1 class="title">Danh sách nhân viên</h1>
-          <button class="add-new-buttonds">Thêm mới</button>
+          <div class="edit-btn-addnv">
+        <a href="themnhanvien.php">Thêm nhân viên</a>
+      </div>
         </div>
       </div>
     </section>
     <!-- Main content -->
     <section id="main-content">
-      <!-- Filter -->
-      <div class="filter-nv">
-        <select name="filterNV" id="filterNV" class="filter">
-          <option value="0">--Chọn điều kiện lọc--</option>
-          <option value="1">Mã Nhân viên</option>
-          <option value="2">Tên Nhân viên</option>
-        </select>
-        <input type="text" id="data" class="filter-input" />
-        <input type="image" src="./icon/nhanvien-search.svg" alt="" class="filter-btn" />
-      </div>
+      <!-- Search -->
+    <form action="?opt=search_nv" method="post" class="filter-nv">
+      <select name="filterNV" id="filterNV" class="filter">
+        <option value="0">--Chọn điều kiện lọc--</option>
+        <option value="1">Mã Sản phẩm</option>
+        <option value="2">Tên Sản phẩm</option>
+      </select>
+      <input type="text" id="data" class="filter-input" />
+      <input type="image" src="./icon/nhanvien-search.svg" alt="" class="filter-btn">
+    </form>
+      
       <!-- Table -->
       <table class="nhanvien-list">
         <thead>
@@ -62,114 +76,184 @@
           </tr>
         </thead>
         <tbody>
-          <!-- NV1 -->
-          <tr>
-            <td>1</td>
-            <td>Nguyễn Văn Bình</td>
-            <td>Nam</td>
-            <td>Đang hoạt động</td>
-            <td>0123456789</td>
-            <td>Quận 5, TPHCM</td>
-            <td>15/01/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/nhanvien-edit.svg" alt="" />
-                <img src="./icon/nhanvien-delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-          <!-- NV2 -->
-          <tr>
-            <td>2</td>
-            <td>Trần Thị Mỹ</td>
-            <td>Nữ</td>
-            <td>Đang hoạt động</td>
-            <td>0987654321</td>
-            <td>Quận 5, TPHCM</td>
-            <td>20/02/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/edit.svg" alt="" />
-                <img src="./icon/delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-          <!-- NV3 -->
-          <tr>
-            <td>3</td>
-            <td>Lê Minh Trường</td>
-            <td>Nam</td>
-            <td>Đang hoạt động</td>
-            <td>0369876543</td>
-            <td>Quận 3, TPHCM</td>
-            <td>10/03/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/edit.svg" alt="" />
-                <img src="./icon/delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-          <!-- NV4 -->
-          <tr>
-            <td>4</td>
-            <td>Phạm Diễm Thi</td>
-            <td>Nữ</td>
-            <td>Đang hoạt động</td>
-            <td>0932468102</td>
-            <td>Quận Gò Vấp, TPHCM</td>
-            <td>05/04/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/edit.svg" alt="" />
-                <img src="./icon/delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-          <!-- NV5 -->
-          <tr>
-            <td>5</td>
-            <td>Nguyễn Ngọc Bích</td>
-            <td>Nữ</td>
-            <td>Đang hoạt động</td>
-            <td>0975312468</td>
-            <td>Quận 7, TPHCM</td>
-            <td>12/05/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/edit.svg" alt="" />
-                <img src="./icon/delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-          <!-- NV6 -->
-          <tr>
-            <td>6</td>
-            <td>Đặng Trần Tấn Minh</td>
-            <td>Nam</td>
-            <td>Đang hoạt động</td>
-            <td>0852369741</td>
-            <td>Quận 7, TPHCM</td>
-            <td>20/06/2023</td>
-            <td>
-              <div class="action">
-                <img src="./icon/edit.svg" alt="" />
-                <img src="./icon/delete.svg" alt="" />
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
 
-      <!-- button>
-        <button>&gt &List -->
-      <div class="list-number">
-        <button>&lt &lt</button>
-        <button>1</button>
-        <button>2</button>
-        <button>...</gt</button>
-        <button>&gt &gt</button>
-      </div>
-    </section>
-  </body>
+        <?php ?>
+        <?php
+        //View
+        function view_nv()
+        {
+          $link = null;
+          taoKetNoi($link);
+          //Xác định trang hiện tại
+          $trang_hien_tai = isset($_GET["trang"]) ? $_GET["trang"] : 1;
+          //Kết nối và lấy dữ liệu từ CSDL
+          $nhanvien_start = ($trang_hien_tai - 1) * 10;
+          $result = chayTruyVanTraVeDL($link, "SELECT s.ma_nhan_vien, s.ho_ten, s.gioi_tinh, s.email, s.so_dien_thoai, s.dia_chi_cu_tru, s.ngay_tham_gia FROM tbl_nhanvien AS s,
+          LIMIT $nhanvien_start, 10");//Giới hạn mỗi trang show 10 sản phẩm
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $row["ma_nhan_vien"] . "</td>";
+            echo "<td>" . $row["ho_ten_"] . "</td>";
+            echo "<td>" . $row["gioi_tinh"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            echo "<td>" . $row["so_dien_thoai"] . "</td>";
+            echo "<td>" . $row["dia_chi_cu_tru"] . "</td>";
+            echo "<td>" . $row["ngay_tham_gia"] . "</td>";
+            echo "<td>";
+            echo "<div class='action'>";
+            echo "<a href='suanhanvien.php?id=" . $row["ma_nhan_vien"] . "'><img src='./icon/nhanvien-edit.svg' alt='Sửa' /></a>";   //Dẫn qua page thêm sửa sản phẩm với tham số mã sản phẩm trên URL 
+            echo "<a href='?opt=del_nv&id=" . $row["ma_nhan_vien"] . "' onclick='return confirm(\"Bạn có chắc chắn muốn xoá nhân vien " . $row["ho_ten"] . "?\");'><img src='./icon/nhanvien-delete.svg' alt='Xóa' /></a>";
+            echo "</div>";
+            echo "</td>";
+            echo "</tr>";
+          }
+          giaiPhongBoNho($link, $result);
+        }
+          //Search
+        function search_nv()
+        {
+          $link = null;
+          taoKetNoi($link);
+          //Kiểm tra có phương thức POST gửi lên hay không - bắt đầu khi click nút search > submit pt POST
+          if (isset($_POST)) {
+            $_filterNV = $_POST["filterNV"];
+            $_giatri = $_POST["giatri"];
+            if ($_filterNV == 0) {
+              //Tạo câu lệnh SQL search mã nhân viên
+              $sql = "SELECT s.ma_nhan_vien, s.ho_ten, s.gioi_tinh, s.email, s.so_dien_thoai, s.dia_chi_cu_tru, s.ngay_tham_gia FROM tbl_nhanvien AS s
+                        AND s.ma_nhan_vien = $_giatri";
+            } else if ($_filterNV == 1) {
+              //Tạo câu lệnh SQL search tên nhân viên
+              $sql = "SELECT s.ma_nhan_vien, s.ho_ten, s.gioi_tinh, s.email, s.so_dien_thoai, s.dia_chi_cu_tru, s.ngay_tham_gia FROM tbl_nhanvien AS s
+               WHERE s.ho_ten LIKE '%" . $_giatri . "%'";
+            }
+            $rs = chayTruyVanTraVeDL($link, $sql);
+            while ($row = mysqli_fetch_assoc($rs)) {
+              if ($row["ma_nhan_vien"] == null) {
+                echo "<script>alert('Sản phẩm không tồn tại!');</script>";
+                echo "<script>window.location.href = 'nhanvien.php?opt=view_nv';</script>";
+              } else {
+                echo "<tr>";
+                echo "<td>" . $row["ma_nhan_vien"] . "</td>";
+                echo "<td>" . $row["ho_ten"] . "</td>";
+                echo "<td>" . $row["gioi_tinh"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+                echo "<td>" . $row["so_dien_thoai"] . "</td>";
+                echo "<td>" . $row["dia_chi_cu_tru"] . "</td>";
+                echo "<td>" . $row["ngay_tham_gia"] . "</td>";
+                echo "<td>
+                    <div class='action'>
+                      <a href='./suanhanvien.php?id='><img src='./icon/nhanvien-edit.svg' alt='Sửa' /></a>
+                      <a href='?opt=del_nv&id=" . $row["ma_nhan_vien"] . "'><img src='./icon/nhanvien-delete.svg' alt='Xóa' /></a>
+                    </div>
+                  </td>";
+                echo "</tr>";
+              }
+            }
+        
+          }
+          giaiPhongBoNho($link, $rs);
+        }
+        // Add
+        function add_nv()
+        {
+          $link = null;
+          taoKetNoi($link);
+          //Kiểm tra có phương thức POST gửi lên hay không
+          if (isset($_POST)) {
+            $_ho_ten = $_POST["hotennv"];
+            $_gioi_tinh = $_POST["gioitinhnv"];
+            $_email = $_POST["emailnv"];
+            $_so_dien_thoai = $_POST["sodienthoainv"];
+            $_dia_chi_cu_tru = $_POST["diachicutru"];
+            $_ngay_tham_gia = $_POST["ngaythamgia"];
+           
+            //Tạo câu lệnh SQL thêm vào bảng nhân viên
+            $sql = "INSERT INTO tbl_nhanvien (ho_ten, gioi_tinh, email, so_dien_thoai, dia_chi_cu_tru, ngay_tham_gia) values ('$_ho_ten','$_gioi_tinh','$_email', '$_so_dien_thoai','$_dia_chi_cu_tru' , '$_ngay_tham_gia')";
+            //Kiểm tra biến tên có dữ liệu hay không
+            if ($_ho_ten != "") {
+              // Thêm nhân viên thành công
+              $rs = chayTruyVanKhongTraVeDL($link, $sql);
+              
+            }
+          }
+          giaiPhongBoNho($link, $rs);
+        }
+        // Update
+        function update_nv()
+        {
+          $link = null;
+          taoKetNoi($link);
+          //Kiểm tra có phương thức POST gửi lên hay không
+          if (isset($_POST)) {
+            $_ho_ten = $_POST["hotennv"];
+            $_gioi_tinh = $_POST["gioitinhnv"];
+            $_email = $_POST["emailnv"];
+            $_so_dien_thoai = $_POST["sodienthoainv"];
+            $_dia_chi_cu_tru = $_POST["diachicutru"];
+            $_ngay_tham_gia = $_POST["ngaythamgia"];
+           
+            }
+            // Xử lý cơ sở dữ liệu 
+            //Cập nhật trước ở bảng nhân viên
+            $sql_nv = "UPDATE tbl_nhanvien SET ho_ten='$_ho_ten', gioi_tinh='$_gioi_tinh', email='$_email', so_dien_thoai='$_so_dien_thoai', dia_chi_cu_tru='$_dia_chi_cu_tru', ngay_tham_gia='$_ngay_tham_gia' where ma_nhan_vien='$_ma_nhan_vien'";
+            $rs = chayTruyVanKhongTraVeDL($link, $sql_nv);
+          giaiPhongBoNho($link, $rs);
+        }
+
+        //Xử lý các option
+        if (isset($_GET["opt"])) {
+          $_opt = $_GET["opt"];
+          switch ($_opt) {
+            case "search_nv":
+              search_nv();
+              break;
+            case "add_nv":
+              add_nv();
+              break;
+            case "update_nv":
+              update_nv();
+              break;
+            case "del_nv":
+              delete_nv();
+              break;
+            default:
+              view_nv();
+          }
+        } else {
+          $_opt = view_nv();
+        }
+        ?>
+      </tbody>
+    </table>
+    <!-- Phân trang -->
+    <div class="list-number">
+      <?php
+      $trang_truoc = 0;
+      $trang_sau = 0;
+      $trang_hien_tai = isset($_GET["trang"]) ? $_GET["trang"] : 1;
+      if ($trang_hien_tai == 1) {
+        $trang_truoc = 1;
+      } else
+        ($trang_truoc = $trang_hien_tai - 1);
+      if ($trang_hien_tai == $so_trang) {
+        $trang_sau = $so_trang;
+      } else
+        ($trang_sau = $trang_hien_tai + 1);
+      ?>
+      <button>
+        <?php echo "<a href='./nhanvien.php?trang=" . $trang_truoc . "'>&lt &lt</a>" ?>
+      </button>
+      <?php
+      for ($i = 1; $i <= $so_trang; $i = $i + 1) {
+        echo "<button><a href='./nhanvien.php?trang=" . $i . "'>$i</a></button>";
+      }
+      ?>
+      <button>
+        <?php echo "<a href='./nhanvien.php?trang=" . $trang_sau . "'>&gt &gt</a>" ?>
+      </button>
+    </div>
+  </section>
+</body>
+
 </html>
