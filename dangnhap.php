d860ade5dcda36f1db4277c790feab321e9abd06
ndnghi24 <dnghitn@gmail.com>
2024-04-11 05:10:42 +0700

    admin login

--- dangnhap.php
+++ dangnhap.php
@@ -11,35 +11,64 @@
     <link rel="stylesheet" href="./css/dangnhap.css">
 </head>
 <body>
+    <!-- Kết nối vào CSDL -->
+    <?php
+        require_once "db_module.php";
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST") {
+            $email = $_POST['email'];
+            $pass = $_POST['pass'];
+            $link = null;
+            taoKetNoi($link);
+            if ($link === false) {
+                die("connection error");
+            }
+
+            $sql="select * from tbl_nhanvien where email='".$email."' AND mat_khau='".$pass."' ";
+            $result=mysqli_query($link,$sql);
+
+            // Kiểm tra số dòng kết quả trả về
+            $num_rows = mysqli_num_rows($result);
+            if ($num_rows == 1) {
+                $row=mysqli_fetch_array($result);
+
+                if($row["role_admin"]=="0") {
+                    echo '<script>alert("Bạn không có quyền truy cập trang này");</script>';
+                } elseif($row["role_admin"]=="1") {
+                    header("location:index.php");
+                    exit(); // Kết thúc kịch bản sau khi chuyển hướng
+                }
+            } else {
+                echo '<script>alert("Email hoặc mật khẩu không đúng");</script>';
+            }
+            // Giải phóng tài nguyên
+            mysqli_close($link);
+        }
+    ?>
     <div class="top">
-        <img
-          class="logo-icon"
-          loading="lazy"
-          alt=""
-          src="./icon/Logo.svg"/>
+        <img class="logo-icon"src="./icon/Logo.svg"/>
         <h1 class="logo-text">Luxe</h1>
     </div>
-
     <!--Fill in the form-->
     <div class="wrapper">
-        <form action="">
+        <form method="post">
             <h1>Đăng nhập</h1>
             <b class="name">
                 <span class="name-text">Email</span>
                 <span class="span"> *</span>
               </b>
             <div class="input-box">
-                <input type="email" required>
+                <input type="email" name="email" required>
             </div>
             <b class="name">
                 <span class="name-text">Mật khẩu</span>
                 <span class="span"> *</span>
               </b>
             <div class="input-box">
-                <input type="password" required>
+                <input type="password" name="pass" required>
             </div>
 
-            <button type="submit" class="btn">Đăng nhập</button>
+            <button type="submit" name="login" class="btn">Đăng nhập</button>
         </form>
     </div>
 </body>
