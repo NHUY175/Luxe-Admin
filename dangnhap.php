b574cd390bf8315e89c350bdc5a65f5b8756f725
ndnghi24 <dnghitn@gmail.com>
2024-04-11 21:15:11 +0700

    admin signin

--- dangnhap.php
+++ dangnhap.php
@@ -13,26 +13,27 @@
 <body>
     <!-- Kết nối vào CSDL -->
     <?php
-        require_once "db_module.php";
-        session_start();
+    require_once "db_module.php";
+    session_start();
 
-        if ($_SERVER["REQUEST_METHOD"]=="POST") {
-            $email = $_POST['email'];
-            $pass = $_POST['pass'];
-            $link = null;
-            taoKetNoi($link);
-            if ($link === false) {
-                die("connection error");
-            }
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        $link = null;
+        taoKetNoi($link);
+        if ($link === false) {
+            die("connection error");
+        }
+        $sql="select * from tbl_nhanvien where email='".$email."'";
+        $result=mysqli_query($link,$sql);
 
-            $sql="select * from tbl_nhanvien where email='".$email."' AND mat_khau='".$pass."' ";
-            $result=mysqli_query($link,$sql);
+        // Kiểm tra số dòng kết quả trả về
+        $num_rows = mysqli_num_rows($result);
+        if ($num_rows == 1) {
+            $row=mysqli_fetch_array($result);
 
-            // Kiểm tra số dòng kết quả trả về
-            $num_rows = mysqli_num_rows($result);
-            if ($num_rows == 1) {
-                $row=mysqli_fetch_array($result);
-
+            if($row["mat_khau"] == $pass) 
+            {
                 if($row["role_admin"]=="0") 
                 {
                     echo '<script>alert("Bạn không có quyền truy cập trang này");</script>';
@@ -43,12 +44,15 @@
                     exit(); // Kết thúc kịch bản sau khi chuyển hướng
                 }
             } else {
-                echo '<script>alert("Email hoặc mật khẩu không đúng");</script>';
+                echo '<script>alert("Sai mật khẩu!");</script>';
             }
-            // Giải phóng tài nguyên
-            mysqli_close($link);
+        } else {
+            echo '<script>alert("Sai email!");</script>';
         }
-    ?>
+        // Giải phóng tài nguyên
+        mysqli_close($link);
+    }
+?>
     <div class="top">
         <img class="logo-icon"src="./icon/Logo.svg"/>
         <h1 class="logo-text">Luxe</h1>
