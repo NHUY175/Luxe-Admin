badccb2dc61ed2b382d152590beca6ecc6cf9b82
Minh Dieu <minhdieu2003@gmail.com>
2024-04-12 01:00:43 +0700

    Update header

--- header.php
+++ header.php
@@ -1,34 +1,40 @@
-<!-- Header -->
-<header>
-      <div class="index_main">
-        <div class="top-bar">
-          <!-- Logo -->
-          <a href="./" class="logo-nav">
-            <img src="./icon/Logo.svg" alt="Luxe" />
-            <h1 class="logo-title">Luxe</h1>
-          </a>
-          <!-- nav = navigation giống div nhưng có ngữ nghĩa -->
-          <!-- Navigation -->
-          <nav class="navbar-index">
-            <ul>
-              <li><a href="index.php">TRANG CHỦ</a></li>
-              <li><a href="sanpham.php">SẢN PHẨM</a></li>
-              <li><a href="ds-khuyenmai.php">KHUYẾN MÃI</a></li>
-              <li><a href="donhang.php">ĐƠN HÀNG</a></li>
-              <li><a href="khachhang.php">KHÁCH HÀNG</a></li>
-              <li><a href="nhanvien.php">NHÂN VIÊN</a></li>
-              <li><a href="thongke-doanhthu.php">THỐNG KÊ</a></li>
-              <li><a href="dangxuat.php">ĐĂNG XUẤT</a></li>
-            </ul>
-          </nav>
+<header id="header">
+<div class="index_main">
+  <div class="top-bar">
+    <!-- Logo -->
+    <a href="./" class="logo-nav">
+      <img src="./icon/Logo.svg" alt="Luxe" />
+      <h1 class="logo-title">Luxe</h1>
+    </a>
+    <!-- nav = navigation giống div nhưng có ngữ nghĩa -->
+    <!-- Navigation -->
+    <nav class="navbar-index">
+      <ul class="menu">
+        <li><a href="./index.php">TRANG CHỦ</a></li>
+        <li>
+          <a href="./sanpham.php">SẢN PHẨM</a>
+          <ul class="sub-menu">
+            <li><a href="./danhmuc.php">DANH MỤC SẢN PHẨM</a></li>
+            <li><a href="./sanpham.php">QUẢN LÝ SẢN PHẨM</a></li>
+          </ul>
+        </li>
+        <li><a href="./ds-khuyenmai.php">KHUYẾN MÃI</a></li>
+        <li><a href="./donhang.php">ĐƠN HÀNG</a></li>
+        <li><a href="./khachhang.php">KHÁCH HÀNG</a></li>
+        <li><a href="./nhanvien.php">NHÂN VIÊN</a></li>
+        <li><a href="./thongke-danhgia.php">THỐNG KÊ</a></li>
+        <li><a href="./dangxuat.php">ĐĂNG XUẤT</a></li>
+      </ul>
+    </nav>
 
-          <!-- Action -->
-          <div class="top-act">
-            <button class="top-act-btn">
-              <img src="./icon/header-user.svg" alt="" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
-
+    <!-- Action -->
+    <div class="top-act">
+      <button class="top-act-btn">
+        <a href="./dangnhap.php"
+          ><img src="./icon/header-user.svg" alt=""
+        /></a>
+      </button>
+    </div>
+  </div>
+</div>
+</header>
